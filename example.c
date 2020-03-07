2b020644e2d7f225e15389ff44460106ecb9657e
alecsmirnov <alecsmirnov93@gmail.com>
2020-03-07 16:00:57 +0700

    accept changes

--- example.c
+++ example.c
@@ -6,10 +6,10 @@ int main(int argc, char* argv[]) {
     int val = 0;
 
     TSQueue* Q = NULL;
-    //tsQueueInit(&Q, TSQUEUE_SIZE_MAX, sizeof(int), NO_FREE_FUNC, TS_NO_WAIT);
+    tsQueueInit(&Q, TSQUEUE_SIZE_MAX, sizeof(int), NO_FREE_FUNC, TS_NO_WAIT);
 
     // Will be blocked at empty queue get
-    tsQueueInit(&Q, TSQUEUE_SIZE_MAX, sizeof(int), NO_FREE_FUNC, TS_WAIT_GET);
+    //tsQueueInit(&Q, TSQUEUE_SIZE_MAX, sizeof(int), NO_FREE_FUNC, TS_WAIT_GET);
     
     val = 1;
     tsQueueSet(Q, (void*)&val);
